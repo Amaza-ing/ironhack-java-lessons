7b9e19de5fce22105775c487c66dfff16c522c23
Adrián Maza <adri_mv1@hotmail.com>
2022-11-08 10:28:41 +0100

    cambio

--- src/com/ironhack/w1d2/Main.java
+++ src/com/ironhack/w1d2/Main.java
@@ -2,6 +2,6 @@ package com.ironhack.w1d2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Funciona!");
+        System.out.println("Esto es un cambio!");
     }
 }
